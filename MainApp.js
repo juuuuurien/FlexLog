05f003d3190b95e2dff13c95e2f776927ae56557
juuuuurien <97487768+juuuuurien@users.noreply.github.com>
2022-05-24 18:05:19 -0700

    Implemented fetch data within MainApp before any screens

--- MainApp.js
+++ MainApp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider } from "react-native-paper";
 
@@ -7,16 +7,23 @@ import WorkoutListNavigator from "./src/screens/WorkoutList/WorkoutListNavigator
 
 import { CombinedDarkTheme, CombinedDefaultTheme } from "./src/theme";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWorkouts } from "./redux/slices/workoutsSlice";
+import { fetchSettings } from "./redux/slices/settingsSlice";
 
 export default function MainApp() {
   const { darkTheme } = useSelector((state) => state.settings.settings);
+  const dispatch = useDispatch();
 
   const getTheme = () => {
     if (darkTheme === true) return CombinedDarkTheme;
-
     return CombinedDefaultTheme;
   };
+
+  useEffect(() => {
+    dispatch(fetchWorkouts());
+    dispatch(fetchSettings());
+  }, []);
 
   return (
     <PaperProvider theme={getTheme()}>
